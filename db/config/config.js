c260181e1bd98feeb4e8b34a56ac5eb177bb4480
pcaldi <pcaldi@gmail.com>
2024-01-16 18:07:25 -0300

    variáveis de ambiente

---
+++ db/config/config.js
@@ -0,0 +1,27 @@
+// Incluir arquivo com variáveis de ambiente
+require('dotenv').config()
+
+// Exportar as credencias do banco de dados
+module.exports = {
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_BASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT
+  }
+}
