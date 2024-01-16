9d63bd559735791b31f8a40ba0f7309684a73115
pcaldi <pcaldi@gmail.com>
2024-01-16 18:06:14 -0300

    Criando models e migrations com Sequelize cli

---
+++ db/models/users.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      //Definir as associações
+      Users.belongsTo(models.Situations, { foreignKey: 'situationId' });
+    }
+  }
+  Users.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    situationId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
