f857ef1ccaa0785626159f93ecb7ba312f4a4c66
pcaldi <pcaldi@gmail.com>
2024-01-31 10:36:59 -0300

    rota editar profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -98,6 +98,117 @@ router.get("/profile", eAdmin, async (req, res) => {
 
 });
 
+// Criar a rota Editar
+// Endereço para acessar a api através de aplicação externa: http://localhost:8080/profile
+// A aplicação externa deve indicar que está enviado os dados em formato de objeto Content-Type: application/json
+// Dados em formato de objeto
+/*
+{
+    "name": "Paulo",
+    "email": "paulo@email.com"
+}
+*/
+router.put("/profile", eAdmin, async (req, res) => {
+
+  //Receber os dados enviados no corpo da requisição
+  const data = req.body;
+
+  // Validar os campos utilizando YUP
+  const schema = yup.object().shape({
+    email: yup.string().email('Error: Necessário preencher com e-mail válido.').required('Error: Necessário preencher com e-mail.'),
+    name: yup.string().required('Error: Necessário preencher o campo nome.')
+  });
+
+  //Verificar se todos os campos passaram pela validação
+  try {
+    await schema.validate(data);
+  } catch (error) {
+    // Retornar objeto como resposta
+    return res.status(401).json({
+      error: true,
+      message: error.errors
+    });
+  }
+
+  // Recuperar o registro no banco de dados
+  const user = await db.Users.findOne({
+
+    //Indicar a coluna para recuperar
+    attributes: ['id'],
+
+    // Acrescentar condição para indicar qual registro deve ser retornado do banco de dados
+    where: {
+      email: data.email,
+      id: {
+        // Operador de negação para ignorar o registro do usuário que está sendo editado
+        [Op.ne]: Number(req.userId) //Id do usuário logado.
+      }
+    }
+
+  });
+
+  // Acessa o IF se encontrar o registo no bando de dados
+  if (user) {
+
+    // Salvar log no nível info
+    logger.info({
+      message: 'Tentativa de utilizar e-mail já cadastrado em outro usuário.',
+      id: req.userId, //Id do usuário logado.
+      name: data.name,
+      email: data.email,
+      situationId: data.situationId,
+      userId: req.userId,
+      date: new Date()
+    });
+
+    // Retorna objeto como resposta
+    return res.status(400).json({
+      error: true,
+      message: 'Error: E-mail já cadastrado!'
+    })
+  };
+
+
+  // Editar no banco de dados
+  await db.Users.update(data, { where: { id: req.userId } }) //Id do usuário logado.
+    .then(() => {
+
+      // Salvar log no nível info
+      logger.info({
+        message: 'Perfil editado com sucesso.',
+        id: req.userId, //Id do usuário logado.
+        name: data.name,
+        email: data.email,
+        userId: req.userId,
+        date: new Date()
+      });
+
+      // Retorno objeto como resposta
+      return res.json({
+        error: false,
+        message: 'Perfil editado com sucesso.'
+      });
+    }).catch(() => {
+
+      // Salvar log no nível info
+      logger.info({
+        message: 'Perfil não editado.',
+        id: req.userId, //Id do usuário logado.
+        name: data.name,
+        email: data.email,
+        userId: req.userId,
+        date: new Date()
+      });
+
+      // Retorno objeto como resposta
+      return res.status(400).json({
+        error: true,
+        message: 'Error: Perfil não editado.'
+      })
+    })
+
+});
+
 
 
 // Exportar a instrução que está dentro da constante router
